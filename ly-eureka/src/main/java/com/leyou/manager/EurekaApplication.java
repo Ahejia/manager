7932402fcaf767df6916257f0c4bc9cf4724641f
Ahejia <951179195@qq.com>
2019-08-29 11:02:39 +0800

    添加名称，描述等信息

--- ly-eureka/src/main/java/com/leyou/manager/EurekaApplication.java
+++ ly-eureka/src/main/java/com/leyou/manager/EurekaApplication.java
@@ -7,14 +7,17 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * @version : 1.0
  * @ClassName: EurekaApplication
- * @Description :
+ * @Description : 注册中心启动类
  * @auther: hejia
  * @date: 2019/3/28
  */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
-
+    /**
+     * @author hejia
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class,args);
     }
